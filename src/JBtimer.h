c024e28124904299372438eaaffa41144864affe
houjingbiao <Jingzhaohu@163.com>
2017-11-25 17:28:52 +0800

    增加获取毫秒的时间

---
+++ src/JBtimer.h
@@ -0,0 +1,45 @@
+#ifndef __JB_TIME_H__
+#define __JB_TIME_H__
+
+#if __linux
+#include <sys/time.h>
+#include <stdio.h>
+#else
+#include <windows.h>
+#include <stdio.h>
+#endif
+
+typedef long long my_time_t;
+
+#if __linux
+my_time_t gettime_millisecond()
+{
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return tv.tv_sec * 1000 + tv.tv_usec / 1000;
+}
+#else
+my_time_t gettime_millisecond()
+{
+	// 获取微秒级别的系统时间
+	SYSTEMTIME tval;
+	GetLocalTime(&tval);
+
+	//printf("%4d/%02d/%02d %02d:%02d:%02d.%03d 星期%1d\n", tval.wYear, tval.wMonth, tval.wDay, tval.wHour, tval.wMinute, tval.wSecond, tval.wMilliseconds, tval.wDayOfWeek);
+
+	// 构造秒级的时间结构
+	struct tm tval_sec;
+	tval_sec.tm_sec = tval.wSecond;
+	tval_sec.tm_min = tval.wMinute;
+	tval_sec.tm_hour = tval.wHour;
+	tval_sec.tm_mday = tval.wDay;
+	tval_sec.tm_mon = tval.wMonth - 1;
+	tval_sec.tm_year = tval.wYear - 1900;
+
+	// 转换获得微秒级别时间
+	my_time_t abc = (my_time_t)mktime(&tval_sec) * 1000 + tval.wMilliseconds;
+	return abc;
+}
+#endif
+
+#endif//__JB_TIME_H__
